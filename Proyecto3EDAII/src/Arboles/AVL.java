5633d105df9116c643f04e967efd06cb50be2821
lued2897 <lued2897@gmail.com>
2023-12-03 16:33:04 -0600

    Menu de usuario

--- Proyecto3EDAII/src/Arboles/AVL.java
+++ Proyecto3EDAII/src/Arboles/AVL.java
@@ -4,6 +4,9 @@
  */
 package Arboles;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author lalox
@@ -14,24 +17,40 @@ public class AVL extends Tree {
         super();
     }
     
+    /**
+     * 
+     * @param data 
+     */
     @Override
     public void add(int data){
-        root = add(root,data,1);
-        root.height = maxHeight(root.left,root.right)+1;
+        if(!search(data)){
+            root = add(root,data);
+            root.height = maxHeight(root.left,root.right)+1;
+        }else{
+            System.out.println("NO SE PERMITEN VALORES REPETIDOS");
+        }
+        
         //root = balance(root);
         
     }
     
-    private Node add(Node root,int data,int height){
+    /**
+     * 
+     * @param root
+     * @param data
+     * @param height
+     * @return 
+     */
+    private Node add(Node root,int data){
         //System.out.println("a");
         if(root == null){
             //return (new Node(data,height)); //test
-            return (new Node(data,1));
+            return (new Node(data,1)); 
         }else if(data < root.data){
-            root.left = add(root.left,data,height+1);
+            root.left = add(root.left,data);
             root.height = maxHeight(root.left,root.right)+1; //test
         }else{
-            root.right = add(root.right,data,height+1);
+            root.right = add(root.right,data);
             root.height = maxHeight(root.left,root.right)+1; //test
         }
         
@@ -42,12 +61,22 @@ public class AVL extends Tree {
         
     }
     
+    /**
+     * 
+     * @param data 
+     */
     @Override
     public void remove(int data){
         root = remove(root,data);
         
     }
     
+    /**
+     * 
+     * @param node
+     * @param data
+     * @return 
+     */
     private Node remove(Node node,int data){
         if(node == null){
             return null;
@@ -76,6 +105,11 @@ public class AVL extends Tree {
         //return node;
     }
    
+    /**
+     * 
+     * @param node
+     * @return 
+     */
     private Node leftRotation(Node node){
         Node r = node.right;
         Node rl = r.left;
@@ -85,7 +119,7 @@ public class AVL extends Tree {
         
         node.height = maxHeight(node.left,node.right)+1;
         if(rl != null){
-            System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            //System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
             rl.height = maxHeight(rl.left,rl.right)+1;
         }   
         r.height = maxHeight(r.left,r.right)+1;
@@ -93,6 +127,11 @@ public class AVL extends Tree {
         return r;
     }
     
+    /**
+     * 
+     * @param node
+     * @return 
+     */
     private Node rightRotation(Node node){
         Node l = node.left;
         Node lr = l.right;
@@ -102,7 +141,7 @@ public class AVL extends Tree {
         
         node.height = maxHeight(node.left,node.right)+1;
         if(lr != null){
-            System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            //System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
             lr.height = maxHeight(lr.left,lr.right)+1;
         }
         l.height = maxHeight(l.left,l.right)+1;
@@ -110,7 +149,12 @@ public class AVL extends Tree {
         return l;
     }
     
-   private int getBalance(Node node){
+    /**
+     * 
+     * @param node
+     * @return 
+     */
+    private int getBalance(Node node){
        if(node == null){
            return 0;
        }else{
@@ -124,9 +168,16 @@ public class AVL extends Tree {
             //return getHeight(node.left)-getHeight(node.right);
        }
    }
-    //Matenme
+    
+    
    //Metodo para balancear la insercion
-   private Node balance(Node node, int data){
+    /**
+     * 
+     * @param node
+     * @param data
+     * @return 
+     */
+    private Node balance(Node node, int data){
        int balance = getBalance(node);
        //System.out.println("data:"+node.data+"  bal:"+balance);
        //this.print(System.out);
@@ -134,30 +185,36 @@ public class AVL extends Tree {
            return node;
        }
        if(balance > 1 && data < node.left.data){
-           System.out.println("Right Rotation");
+           //System.out.println("Right Rotation");
            return rightRotation(node);
            
        }else if(balance < -1 && data > node.right.data){
-           System.out.println("Left Rotation");
+           //System.out.println("Left Rotation");
            return leftRotation(node);
            
        }else if(balance > 1 && data > node.left.data){
-           System.out.println("Left Rotation");
+           //System.out.println("Left Rotation");
            node.left = leftRotation(node.left);
-           System.out.println("Right Rotation");
+           //System.out.println("Right Rotation");
            return rightRotation(node);
            
        }else if(balance < -1 && data < node.right.data){
-           System.out.println("Right Rotation");
+           //System.out.println("Right Rotation");
            node.right = rightRotation(node.right);
-           System.out.println("Left Rotation");
+           //System.out.println("Left Rotation");
            return leftRotation(node);
             
        }
        return node;
    }
    
-   private Node balance(Node node){
+   //Metodo para balancear la eliminacion
+    /**
+     * 
+     * @param node
+     * @return 
+     */
+    private Node balance(Node node){
        int balance = getBalance(node);
        //System.out.println("data:"+node.data+"  bal:"+balance);
        //this.print(System.out);
@@ -165,34 +222,45 @@ public class AVL extends Tree {
            return node;
        }
        if(balance > 1 && getBalance(node.left) >=0){
-           System.out.println("Right Rotation");
+           //System.out.println("Right Rotation");
            return rightRotation(node);
            
        }else if(balance < -1 && getBalance(node.right) <= 0){
-           System.out.println("Left Rotation");
+           //System.out.println("Left Rotation");
            return leftRotation(node);
            
        }else if(balance > 1 && getBalance(node.left) < 0){
-           System.out.println("Left Rotation");
+           //System.out.println("Left Rotation");
            node.left = leftRotation(node.left);
-           System.out.println("Right Rotation");
+           //System.out.println("Right Rotation");
            return rightRotation(node);
            
        }else if(balance < -1 && getBalance(node.right) > 0){
-           System.out.println("Right Rotation");
+           //System.out.println("Right Rotation");
            node.right = rightRotation(node.right);
-           System.out.println("Left Rotation");
+           //System.out.println("Left Rotation");
            return leftRotation(node);
             
        }
        return node;
    }
    
-   public boolean search(int data){
+    /**
+     * 
+     * @param data
+     * @return 
+     */
+    public boolean search(int data){
        return search(this.root,data);
-   }
+    }
    
-   private boolean search(Node node,int data){
+    /**
+     * 
+     * @param node
+     * @param data
+     * @return 
+     */
+    private boolean search(Node node,int data){
        if(node == null){
             return false;
         }else if(data == node.data){
@@ -206,8 +274,92 @@ public class AVL extends Tree {
         }
    }
    
-   //Balanceo para la eliminacion
-   public static void main(String[] args) {
+    /**
+     * 
+     */
+    public void menu(){
+        AVL tree = new AVL();
+        int opt=0;
+        int input;
+        while(opt !=-1){
+            opciones();
+            System.out.print("Seleccione una opcion: ");
+            opt = readInt();
+            System.out.println();
+            
+            switch(opt){
+                case 0:
+                    opciones();
+                    break;
+                    
+                case 1:
+                    System.out.print("Ingrese el valor de la nueva clave: ");
+                    input = readInt();
+                    System.out.println();
+                    tree.add(input);
+                    break;
+                
+                case 2:
+                    System.out.print("Ingrese el valor de la clave a eliminar: ");
+                    input = readInt();
+                    System.out.println();
+                    tree.remove(input);
+                    break;    
+                
+                case 3:
+                    System.out.print("Ingrese el valor de la clave: ");
+                    input = readInt();
+                    System.out.println();
+                    if(tree.search(input)){
+                        System.out.println(input+" Se encuentar en el arbol");
+                    }else{
+                        System.out.println(input+" NO se encuentra en el arbol");
+                    }
+                    break; 
+                    
+                case 4:
+                    System.out.println("Arbol AVL:\n");
+                    tree.print(System.out);
+                    break;
+                    
+                case 5:
+                    tree = new AVL();
+                    break;
+            }
+        }
+    }
+    
+    private void opciones(){
+        System.out.println("(0) Mostrar opciones");
+        System.out.println("(1) Agregar clave");
+        System.out.println("(2) Eliminar clave");
+        System.out.println("(3) Buscar clave");
+        System.out.println("(4) Mostrar arbol");
+        System.out.println("(5) Nuevo arbol");
+    }
+    
+    private int readInt(){
+        int val;
+        while(true){
+            try{
+                Scanner scan = new Scanner(System.in);
+                val = scan.nextInt();
+                return val;
+            }catch(InputMismatchException e){
+                //System.out.println("e.getMessage;");
+                System.out.println("Solo se permiten enteros");
+            }    
+        }
+        
+        
+    }
+    
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
         int values[] = {50,30,20,40,70,60,80,15,25,35,45,55,65,75,85,72,78,71,73};
         //int values[]={50,30,20};
         /*
@@ -245,6 +397,8 @@ public class AVL extends Tree {
         System.out.println(tree.search(78));
         System.out.println(tree.search(25));
         tree.print(System.out);
+        
+        tree.menu();
     
         
     }
